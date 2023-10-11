ad274ddc4e22dacf06254e4c6a8ed3a1c5cdc965
ahmedbasha10 <bashamido6060@gmail.com>
2023-10-11 12:46:29 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 import { CartProvider, NotificationProvider } from "./Utils/Context";
@@ -11,12 +11,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <NotificationProvider>
           <App />
         </NotificationProvider>
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
